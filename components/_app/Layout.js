b366102af6cd45b8b314c78d16c8c8e8cba3c3e5
Andrew Umstead <andrewumstead@MacBook-Air-2.local>
2020-07-03 16:11:38 +0800

    add google analytics

--- components/_app/Layout.js
+++ components/_app/Layout.js
@@ -1,21 +1,34 @@
-import Head from 'next/head'
-import Navbar from './Navbar'
-import HeadContent from './HeadContent'
-import styles from './Layout.module.scss'
+import Head from "next/head";
+import Navbar from "./Navbar";
+import HeadContent from "./HeadContent";
+import styles from "./Layout.module.scss";
 
 function Layout({ children, user }) {
   return (
     <>
       <Head>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171572949-1"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'UA-171572949-1');
+            </script>            
+              `,
+          }}
+        />
         <HeadContent />
         <title>dgi folios - Dividend investing, tracking, and sharing.</title>
       </Head>
       <Navbar user={user} />
-      <div className={styles.layoutDiv}>
-        {children}
-      </div>
+      <div className={styles.layoutDiv}>{children}</div>
     </>
-  )
+  );
 }
 
 export default Layout;
